61ade2b82b3333da3a02b9f77a12fb608412bab1
Sarah Wilson <sarah-janewilson@live.co.uk>
2023-05-30 16:24:18 +0100

    Added single review page which displays when a user clicks the link on the review list

--- src/api.js
+++ src/api.js
@@ -1,12 +1,19 @@
 import axios from "axios";
 
 const gamesApi = axios.create({
-    baseURL: `https://sarah-nc-games.onrender.com/api`,
-  });
+  baseURL: `https://sarah-nc-games.onrender.com/api`,
+});
 
-  export const fetchReviews = () => {
-    return gamesApi
-      .get(`/reviews`)
-      .then((response) => response.data)
-      .catch((err) => console.log(err));
-  };
+export const fetchReviews = () => {
+  return gamesApi
+    .get(`/reviews`)
+    .then((response) => response.data)
+    .catch((err) => console.log(err));
+};
+
+export const fetchSingleReview = (review_id) => {
+  return gamesApi
+    .get(`/reviews/${review_id}`)
+    .then((response) => response.data)
+    .catch((err) => console.log(err));
+};
